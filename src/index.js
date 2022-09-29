b68684ffeea2583ab6ee827603256fccad622d68
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-09-29 20:38:47 +0600

    Favicon + Title

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
